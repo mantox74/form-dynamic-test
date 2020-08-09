c3a494a21b9fadffcb0713140125f0af25cd5919
Massimiliano Mantovani <massimiliano.mantovani@noovle.com>
2020-08-09 19:25:02 +0200

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent ],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		ReactiveFormsModule,
+		BrowserAnimationsModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatDividerModule,
+		MatGridListModule
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/models/base-formcontrol.ts
+++
@@ -1,9 +0,0 @@
-export class BaseFormcontrol<T> {
-	name: string;
-	type: string;
-	required: boolean;
-	readOnly: boolean;
-	label: string;
-
-	constructor() {}
-}
